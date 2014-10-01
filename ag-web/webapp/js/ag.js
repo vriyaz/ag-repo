fb47e69fb38478da8b8d46c303829536a68857ca
vriyaz <vriyaz@gmail.com>
2014-10-02 01:59:18 +0530

    added a footer to all pages excluding the jasmine test pages

---
+++ ag-web/webapp/js/ag.js
@@ -0,0 +1,10 @@
+(function () {
+    var app = angular.module('AgApp', []);
+
+    app.service('AgService', function() {
+    });
+                 
+    app.controller('AgController', ['$scope', '$http', 'AgService', function ($scope, $http, service) {
+    }]);
+ 
+})();
